783525741b1102fffae7ac4c2a884d8d4278e75a
Shougan Gu <dashoug001@gmail.com>
2024-08-12 17:28:33 -0400

    Unloaded brawlers issue and reorganised localstorage

--- functions.js
+++ functions.js
@@ -43,12 +43,10 @@ function tdarkmode(){
 }
 
 function hidemenu(){
-    /**
-      Function is used to toggle the menu on and off.
-     */
-    if(!menuHid){
+    menuHid = localStorage.getItem("menuHid");
+    if(menuHid == 'false'){
+        console.log(1)
         menuHid = true
-        localStorage.setItem("menuHid", "1");
         document.getElementById("top").style.height = "3.5vh";
         document.getElementById("logoarr").style.rotate = "180deg";
         var x = document.getElementsByClassName("logobutton");
@@ -64,8 +62,8 @@ function hidemenu(){
         document.getElementById("navmain").style.height = "0vh";
     }
     else{
+        console.log(2)
         menuHid = false
-        localStorage.setItem("menuHid", "0");
         document.getElementById("top").style.height = "7vh";
         document.getElementById("logoarr").style.rotate = "0deg";
         var x = document.getElementsByClassName("logobutton");
@@ -80,7 +78,11 @@ function hidemenu(){
         document.getElementById("navmain").style.bottom = "0vh";
         document.getElementById("navmain").style.height = "2vh";
     }
+
+    localStorage.setItem("menuHid", menuHid);
+
 }
+
 
 function checkSize(){
     /**
@@ -108,13 +110,13 @@ function checkSize(){
     }
 }
 
-function animate_endscreen(){
+function animate_endscreen(brawlerName){
     var shadow = document.getElementById("shadow");
     var endscreen =  document.getElementById("endscreen");
     shadow.style.display = "inherit";
     endscreen.style.display = "inherit";
 
-    document.getElementById("endscreen-img").src = "images/" + brawlers[m_brawler].name + ".png";
+    document.getElementById("endscreen-img").src = "images/" + brawlerName + ".png";
 
     animateCSS(shadow, "fadeIn", "2s");
     animateCSS(endscreen, "fadeInDown", "2s");
@@ -140,8 +142,6 @@ function animateCSS(element, animation, delay, prefix = 'animate__'){
 
 
 
-
-
 function toggleKeyboard(){
     if(keyboard){
         document.getElementById("keyboard1").style.display = "none";
@@ -160,4 +160,48 @@ function toggleKeyboard(){
         document.getElementById("keyboardTop").style.bottom = "36vh";
     }
     keyboard = !keyboard;
+}
+
+function autocomplete(bName){
+    var input = document.getElementById("input");
+    input.value = bName;
+}
+
+function updateSuggestions(){
+    var input = document.getElementById("input");
+    var sList = document.getElementById("brawlerSuggestion");
+
+    sList.innerHTML = "";
+    for(var i=0; i<=brawlers.length-1; i++){
+        if(brawlers[i].name.toLowerCase().includes(input.value.toLowerCase())){
+            var bNameFix = '"'+brawlers[i].name+'"';
+            sList.innerHTML += "<img src='"+ brawlers[i].name +".png' class='sBrawler' onclick='autocomplete("+ bNameFix +")'>";
+        }
+    }
+}
+
+function kbPress(character, shouldUpdateBrawler = true){
+    var input = document.getElementById("input");
+    input.value += character;
+    if (shouldUpdateBrawler){
+    updateSuggestions();
+    }
+}
+
+function kbBack(shouldUpdateBrawler = true){
+    var input = document.getElementById("input");
+    input.value = input.value.slice(0, -1); 
+    if (shouldUpdateBrawler){
+    updateSuggestions();}
+}
+function autocorrect(guess_text){
+    if(guess_text == "rt" || guess_text == "r t")
+        guess_text = "r-t";
+    if(guess_text == "mrp" || guess_text == "mr p" || guess_text == "mr.p")
+        guess_text = "mr. p";
+    if(guess_text == "8bit" || guess_text == "8 bit")
+        guess_text = "8-bit";
+    if(guess_text == "larry" || guess_text == "lawrie" || guess_text == "larry lawrie" || guess_text == "larry&lawrie")
+        guess_text = "larry & lawrie";
+    return guess_text;
 }
